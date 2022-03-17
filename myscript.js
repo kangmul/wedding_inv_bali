6f95248751016eb607addf2aa4c011f0676ffc90
LAPTOP-8EUNS5KJ\cbn <muluk.dharmayana@gmail.com>
2022-03-17 14:28:26 +0700

    invitation bali sample

---
+++ myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#bukaundangan").on("click", function () {
+    $("#splash-screen").addClass("animate__animated animate__fadeOutUp");
+    $("#img-cover").addClass("animate__animated animate__fadeOutUp");
+    // $("body").addClass("animate__animated animate__bounceOutUp");
+    // $("body").css("background-image", "none");
+  });
+});
